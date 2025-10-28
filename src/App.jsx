36ceaf72d74b849b8cbda4a18e8b9f13895679d5
Erwan <erwan.a.marechal@gmail.com>
2025-10-28 15:02:00 +0100

    feat: setup react-router and main App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,32 @@
-<h1>Hello World</h1>
+import { Routes, Route } from 'react-router-dom';
+
+// Importing components for different routes
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+// Importing pages
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-100 text-white">
+        <Navbar />
+        <main className="flex-grow">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/produits" element={<Products />} />
+                <Route path="/produits/:id" element={<ProductDetail />} />
+                <Route path="/a-propos" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </main>
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
